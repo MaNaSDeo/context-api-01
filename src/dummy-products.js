84ad29fbb33dcc4b8c38f6ad18f87d8c8d6d8531
MaNaSDeo <102231033+MaNaSDeo@users.noreply.github.com>
2023-12-27 11:39:07 +0530

    All change done

--- src/dummy-products.js
+++ src/dummy-products.js
@@ -22,7 +22,6 @@ export const DUMMY_PRODUCTS = [
     description:
       'Bask in the glow of elegance with our Enchanting Blush Dream Gown. Embody the regality of a queen with a sweet touch of pink that whispers enchantment. This is the perfect piece for those seeking to create unforgettable moments.',
   },
-
   {
     id: 'p3',
     image: rainJacket,
@@ -38,21 +37,21 @@ export const DUMMY_PRODUCTS = [
     price: 249.99,
     description:
     'Step into the boardroom with unmatched confidence in our Classic Merlot Business Suit. Exuding an air of refined class and understated power, it is ideal for the modern executive who values tradition and elegance.',
-    },
-    {
+  },
+  {
     id: 'p5',
     image: moonlightDress,
     title: 'Ethereal Moonlight Evening Dress',
     price: 159.99,
     description:
     'Sweep the room off its feet in our Ethereal Moonlight Evening Dress. Crafted to mimic the allure of the moonlight, this dress is a nod to those who appreciate subtle glamour and a standout silhouette.',
-    },
-    {
+  },
+  {
     id: 'p6',
     image: denimPioneer,
     title: 'Pioneer Rugged Denim Jacket',
     price: 79.99,
     description:
     'Our Pioneer Rugged Denim Jacket is a tribute to those who embody the spirit of adventure. Designed with durability and comfort in mind, this jacket is a wardrobe essential for the urban explorer.'
-    }
+  }
 ];
